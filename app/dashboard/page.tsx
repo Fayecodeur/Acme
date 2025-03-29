e74c907986b4ed23fe9df3f1023f71c40fc6e45e
Fayecodeur <fayecodeur99@gmail.com>
2025-03-29 16:48:04 +0000

    affichages des données dans les cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,12 @@ import Card from "../ui/Card";
 import { FetchCardData } from "../lib/data";
 
 export default async function Dashboard() {
-  const { totalPaidInvoices } = await FetchCardData();
+  const {
+    totalPaidInvoices,
+    totalPendigInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await FetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl `}>
@@ -11,6 +16,17 @@ export default async function Dashboard() {
       </h1>
       <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
         <Card title="Collecté" value={totalPaidInvoices} type="collected" />
+        <Card title="En attente" value={totalPendigInvoices} type="pending" />
+        <Card
+          title="Toatal factures"
+          value={numberOfInvoices}
+          type="invoices"
+        />
+        <Card
+          title="Toatal clients"
+          value={numberOfCustomers}
+          type="customers"
+        />
       </div>
     </main>
   );
