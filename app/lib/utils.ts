55aa7f2518dc86ad1e9e98d06fd550324600d4c6
Fayecodeur <fayecodeur99@gmail.com>
2025-03-29 18:59:57 +0000

    ajout du fonctions pour calculer la hauteur du graphique

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,16 @@
+import { Revenue } from "./definitions";
 export const formatCurrency = (amount: number) => {
   return (amount / 100).toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
   });
+};
+
+export const generateYaxis = (revenue: Revenue[]) => {
+  if (revenue.length === 0) return 1000; // Valeur par défaut pour éviter Math.max([])
+
+  const highestRecord = Math.max(...revenue.map((month) => month.revenue));
+  const topLabel = Math.ceil(highestRecord / 1000) * 1000;
+
+  return topLabel;
 };
