7b404195b3faa48f5d28de94eda3d9ec61f1ef31
Fayecodeur <fayecodeur99@gmail.com>
2025-03-29 16:22:05 +0000

    affichage du montant collecté

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,17 @@
-export default function Dashboard() {
-  return <div>Tableau de bord</div>;
+import { lusitana } from "@/app/ui/font";
+import Card from "../ui/Card";
+import { FetchCardData } from "../lib/data";
+
+export default async function Dashboard() {
+  const { totalPaidInvoices } = await FetchCardData();
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl `}>
+        Tableau de bord
+      </h1>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <Card title="Collecté" value={totalPaidInvoices} type="collected" />
+      </div>
+    </main>
+  );
 }
