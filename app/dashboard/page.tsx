cc4ec11a8eb4c6ff0c5e712b3a60354405f4009a
Fayecodeur <fayecodeur99@gmail.com>
2025-03-30 15:37:15 +0000

    affichage de la liste des 5 derniers clients

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,16 @@
 import { lusitana } from "@/app/ui/font";
-import Card from "../ui/Card";
-import { FetchCardData, fetchRevenue } from "@/app/lib/data";
-import RevenueChart from "@/app/ui/revenue-chart";
+import Card from "@/app/ui/dashboard/Card";
+import {
+  FetchCardData,
+  fetchRevenue,
+  fetchLatestInvoices,
+} from "@/app/lib/data";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/LatestInvoices";
 
 export default async function Dashboard() {
   const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendigInvoices,
@@ -19,19 +25,16 @@ export default async function Dashboard() {
       <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
         <Card title="Collecté" value={totalPaidInvoices} type="collected" />
         <Card title="En attente" value={totalPendigInvoices} type="pending" />
+        <Card title="Total factures" value={numberOfInvoices} type="invoices" />
         <Card
-          title="Toatal factures"
-          value={numberOfInvoices}
-          type="invoices"
-        />
-        <Card
-          title="Toatal clients"
+          title="Total clients"
           value={numberOfCustomers}
           type="customers"
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
