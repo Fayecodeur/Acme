5af72341fbaed06dd4894975acae3adbd1dc2bf6
Fayecodeur <fayecodeur99@gmail.com>
2025-04-04 17:25:03 +0000

    ajout du skeleton

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,12 @@
 import { lusitana } from "@/app/ui/font";
 import Card from "@/app/ui/dashboard/Card";
-import {
-  FetchCardData,
-  fetchRevenue,
-  fetchLatestInvoices,
-} from "@/app/lib/data";
+import { FetchCardData, fetchLatestInvoices } from "@/app/lib/data";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/LatestInvoices";
+import { Suspense } from "react";
+import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function Dashboard() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
@@ -33,7 +30,9 @@ export default async function Dashboard() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
