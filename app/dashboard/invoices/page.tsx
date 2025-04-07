8f60a06feea205947063f825439bbd0d0dcae54a
Fayecodeur <fayecodeur99@gmail.com>
2025-04-07 15:19:08 +0000

    affichage de la barre de recherche

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import { lusitana } from "@/app/ui/font";
+import CreateInvoices from "@/app/ui/invoices/Buttons";
+import Search from "@/app/ui/Search";
 
-export default function Factures() {
-  return <div>Factures</div>;
+export default async function Factures() {
+  return (
+    <div className="w-full">
+      <div className="w-full flex items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Factures</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Rechercher des factures" />
+        <CreateInvoices />
+      </div>
+    </div>
+  );
 }
