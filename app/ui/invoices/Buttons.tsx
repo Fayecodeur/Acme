51fa7568010f73bd9f288bbe94ee3226e78146f2
Fayecodeur <fayecodeur99@gmail.com>
2025-04-28 17:59:17 +0000

    Création du boutton pour la modification

--- app/ui/invoices/Buttons.tsx
+++ app/ui/invoices/Buttons.tsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from "@heroicons/react/24/outline";
+import { PencilIcon, PlusIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 export default function CreateInvoices() {
   return (
@@ -8,6 +8,17 @@ export default function CreateInvoices() {
     >
       <span className="hidden md:block">Créer une facture</span>
       <PlusIcon className="h-5 md:ml-4" />
+    </Link>
+  );
+}
+
+export function UpdateInvoice({ id }: { id: string }) {
+  return (
+    <Link
+      href={`/dashboard/invoices/${id}/edit`}
+      className="rounded-md border p-2 hover:bg-gray-100"
+    >
+      <PencilIcon className="w-5" />
     </Link>
   );
 }
