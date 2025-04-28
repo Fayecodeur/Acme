11e819bef0add16cc030c6c20e13e1ae224052e6
Fayecodeur <fayecodeur99@gmail.com>
2025-04-28 15:17:25 +0000

    Amélioaratin du formualire

--- app/ui/invoices/CreateForm.tsx
+++ app/ui/invoices/CreateForm.tsx
@@ -23,7 +23,7 @@ export default function CreateForm() {
             <select
               id="customer"
               name="customerId"
-              className="peer block w-full cursor-pointer rounded-md border border-gray-100 bg-white py-2 pl-10 pr-3 text-sm text-gray-700 placeholder:text-gray-400 shadow-sm focus:border-blue-400 focus:ring-1 focus:ring-blue-300"
+              className="peer block w-full rounded-md border border-gray-200 bg-white py-2 pl-10 pr-3 text-sm text-gray-700 placeholder:text-gray-400 shadow-sm focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
               defaultValue=""
             >
               <option value="" disabled>
@@ -50,7 +50,7 @@ export default function CreateForm() {
               type="number"
               step="0.01"
               placeholder="Entrez un montant en USD"
-              className="peer block w-full rounded-md border border-gray-100 bg-white py-2 pl-10 pr-3 text-sm text-gray-700 placeholder:text-gray-400 shadow-sm focus:border-blue-400 focus:ring-1 focus:ring-blue-300"
+              className="peer block w-full rounded-md border border-gray-200 bg-white py-2 pl-10 pr-3 text-sm text-gray-700 placeholder:text-gray-400 shadow-sm focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
             />
             <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-400 peer-focus:text-blue-500" />
           </div>
