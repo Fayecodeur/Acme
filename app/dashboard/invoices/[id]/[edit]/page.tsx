2cf6f45d2a5e7bfeb3f699227901fbcfb49d3e88
Fayecodeur <fayecodeur99@gmail.com>
2025-04-29 13:19:45 +0000

    Affichage du formulaire

--- app/dashboard/invoices/[id]/[edit]/page.tsx
+++ app/dashboard/invoices/[id]/[edit]/page.tsx
@@ -1,3 +1,26 @@
-export default async function Edit() {
-  return <div>page</div>;
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/Breadcrumbs";
+import EditForm from "@/app/ui/invoices/EditForm";
+
+export default async function Edit({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const [invoice, customer] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Factures", href: "/dashboard/invoices" },
+          {
+            label: "Modifier la factures",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditForm invoice={invoice} customers={customer} />
+    </main>
+  );
 }
