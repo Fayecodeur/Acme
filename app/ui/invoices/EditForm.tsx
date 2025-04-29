c5b6b16f7a00120423edcb5ab639642c86eb1a9f
Fayecodeur <fayecodeur99@gmail.com>
2025-04-29 13:18:13 +0000

    creation du formualaire de modification

---
+++ app/ui/invoices/EditForm.tsx
@@ -0,0 +1,127 @@
+import { CustomerField, InvoiceForm } from "@/app/lib/definitions";
+import { Button } from "@/app/ui/Button";
+import {
+  CheckIcon,
+  ClockIcon,
+  CurrencyDollarIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+export default function EditForm({
+  customers,
+  invoice,
+}: {
+  customers: CustomerField[];
+  invoice: InvoiceForm;
+}) {
+  return (
+    <form>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6 shadow-sm">
+        {/* Nom du client */}
+        <div className="mb-4">
+          <label
+            htmlFor="customer"
+            className="mb-2 block text-sm font-medium text-gray-700"
+          >
+            Sélectionner un client
+          </label>
+          <div className="relative">
+            <select
+              id="customer"
+              name="customerId"
+              className="peer block w-full rounded-md border border-gray-200 bg-white py-2 pl-10 pr-3 text-sm text-gray-700 placeholder:text-gray-400 shadow-sm focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+              defaultValue={invoice.customer_id}
+            >
+              <option value="" disabled>
+                Choisir un client
+              </option>
+              {customers.map((customer) => (
+                <option key={customer.id} value={customer.id}>
+                  {customer.name}
+                </option>
+              ))}
+            </select>
+            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-400" />
+          </div>
+        </div>
+
+        {/* Montant de la facture */}
+        <div className="mb-4">
+          <label
+            htmlFor="amount"
+            className="mb-2 block text-sm font-medium text-gray-700"
+          >
+            Choisir un montant
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <input
+              id="amount"
+              name="amount"
+              type="number"
+              defaultValue={invoice.amount}
+              step="0.01"
+              placeholder="Entrez un montant en USD"
+              className="peer block w-full rounded-md border border-gray-200 bg-white py-2 pl-10 pr-3 text-sm text-gray-700 placeholder:text-gray-400 shadow-sm focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+            />
+            <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-400 peer-focus:text-blue-500" />
+          </div>
+        </div>
+
+        {/* Statut de la facture */}
+        <fieldset>
+          <legend className="mb-2 block text-sm font-medium text-gray-700">
+            Définir le statut de la facture
+          </legend>
+          <div className="rounded-md border border-gray-100 bg-white px-[14px] py-3 shadow-sm">
+            <div className="flex gap-4">
+              <div className="flex items-center">
+                <input
+                  id="pending"
+                  name="status"
+                  type="radio"
+                  value="pending"
+                  defaultChecked={invoice.status === "pending"}
+                  className="h-4 w-4 cursor-pointer border-gray-300 text-blue-500 focus:ring-blue-400"
+                />
+                <label
+                  htmlFor="pending"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
+                >
+                  En attente <ClockIcon className="h-4 w-4" />
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="paid"
+                  name="status"
+                  defaultChecked={invoice.status === "paid"}
+                  type="radio"
+                  value="paid"
+                  className="h-4 w-4 cursor-pointer border-gray-300 text-green-600 focus:ring-green-400"
+                />
+                <label
+                  htmlFor="paid"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
+                >
+                  Payé <CheckIcon className="h-4 w-4" />
+                </label>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+      </div>
+
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard/invoices"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Annuler
+        </Link>
+        <Button type="submit" chilildren={undefined}>
+          Modifier la facture
+        </Button>
+      </div>
+    </form>
+  );
+}
