dcc1a628908fa5fa61127cfc403673c678be64da
Fayecodeur <fayecodeur99@gmail.com>
2025-05-01 13:02:56 +0000

    Gestion des erreurs

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,8 +25,12 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`INSERT INTO invoices (customer_id, amount, status, date)
- VALUES(${customerId}, ${amountInCents}, ${status}, ${date})`;
+  try {
+    await sql`INSERT INTO invoices (customer_id, amount, status, date)
+              VALUES(${customerId}, ${amountInCents}, ${status}, ${date})`;
+  } catch (error) {
+    return { message: "Erreur lors de la création de la facture" };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -41,17 +45,25 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices 
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    return { message: "Erreur lors de la mise à jour de la facture" };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath("/dashboard/invoices");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+  } catch (error) {
+    return { message: "Erreur lors de la suppréssion de la facture" };
+  }
 }
