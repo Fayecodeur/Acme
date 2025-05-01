d6bd2d545faedb700b8a18ab79c1395d2c045e76
Fayecodeur <fayecodeur99@gmail.com>
2025-05-01 16:15:58 +0000

    Validation des données

--- app/ui/invoices/CreateForm.tsx
+++ app/ui/invoices/CreateForm.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { createInvoice } from "@/app/lib/actions";
 import { CustomerField } from "@/app/lib/definitions";
 import { Button } from "@/app/ui/Button";
@@ -8,13 +9,19 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useActionState } from "react";
 export default function CreateForm({
   customers,
 }: {
   customers: CustomerField[];
 }) {
+  const initialState = {
+    message: "",
+    error: {},
+  };
+  const [state, formAction] = useActionState(createInvoice, initialState);
   return (
-    <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6 shadow-sm">
         {/* Nom du client */}
         <div className="mb-4">
@@ -30,6 +37,7 @@ export default function CreateForm({
               name="customerId"
               className="peer block w-full rounded-md border border-gray-200 bg-white py-2 pl-10 pr-3 text-sm text-gray-700 placeholder:text-gray-400 shadow-sm focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
               defaultValue=""
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Choisir un client
@@ -41,6 +49,14 @@ export default function CreateForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-400" />
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.error?.customerId &&
+              state.error.customerId.map((error: string) => (
+                <p key={error} className="mt-2 text-sm text-red-500">
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -60,8 +76,17 @@ export default function CreateForm({
               step="0.01"
               placeholder="Entrez un montant en USD"
               className="peer block w-full rounded-md border border-gray-200 bg-white py-2 pl-10 pr-3 text-sm text-gray-700 placeholder:text-gray-400 shadow-sm focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+              aria-describedby="amount-error"
             />
             <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-400 peer-focus:text-blue-500" />
+          </div>
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.error?.amount &&
+              state.error.amount.map((error: string) => (
+                <p key={error} className="mt-2 text-sm text-red-500">
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -78,6 +103,7 @@ export default function CreateForm({
                   name="status"
                   type="radio"
                   value="pending"
+                  aria-describedby="status-error"
                   className="h-4 w-4 cursor-pointer border-gray-300 text-blue-500 focus:ring-blue-400"
                 />
                 <label
@@ -93,6 +119,7 @@ export default function CreateForm({
                   name="status"
                   type="radio"
                   value="paid"
+                  aria-describedby="status-error"
                   className="h-4 w-4 cursor-pointer border-gray-300 text-green-600 focus:ring-green-400"
                 />
                 <label
@@ -104,7 +131,20 @@ export default function CreateForm({
               </div>
             </div>
           </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.error?.status &&
+              state.error.status.map((error: string) => (
+                <p key={error} className="mt-2 text-sm text-red-500">
+                  {error}
+                </p>
+              ))}
+          </div>
         </fieldset>
+        <div aria-live="polite" aria-atomic="true">
+          {state.message ? (
+            <p className="mt-2 text-sm text-red-500">{state.message}</p>
+          ) : null}
+        </div>
       </div>
 
       <div className="mt-6 flex justify-end gap-4">
