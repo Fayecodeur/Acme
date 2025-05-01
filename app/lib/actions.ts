03a8d9ef70a1f5a8dc33d448048f15ef4d66e29e
Fayecodeur <fayecodeur99@gmail.com>
2025-05-01 16:15:47 +0000

    Validation des données

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,21 +7,46 @@ import { z } from "zod";
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
+  customerId: z.string({
+    invalid_type_error: "Veuillez sélectionner un client",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Veuillez entrer un montant supérieur à $0" }),
   date: z.string(),
-  status: z.enum(["paid", "pending"]),
+  status: z.enum(["paid", "pending"], {
+    invalid_type_error: "Veuillez sélectionner le status de la facture",
+  }),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateteInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  error?: {
+    cuscustomerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+  if (!validatedFields.success) {
+    return {
+      error: validatedFields.error.flatten().fieldErrors,
+      message: "Champs manquants. Echec lors de la création de la facture",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
